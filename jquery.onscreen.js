31962f31f294cdd7c655c1a31726dad72c464826
Jon Bevan <jon@jonbevan.me.uk>
2012-06-22 12:25:00 +0100

    Be a good citizen. Use one var and many semicolons.

--- jquery.onscreen.js
+++ jquery.onscreen.js
@@ -6,17 +6,17 @@
 // Released under MIT license.
 ;(function($) {
   $.expr[":"].onScreen = function(elem) {
-    var $window = $(window)
-    var viewport_top = $window.scrollTop()
-    var viewport_height = $window.height()
-    var viewport_bottom = viewport_top + viewport_height
-    var $elem = $(elem)
-    var top = $elem.offset().top
-    var height = $elem.height()
-    var bottom = top + height
+    var $window = $(window),
+    	viewport_top = $window.scrollTop(),
+    	viewport_height = $window.height(),
+    	viewport_bottom = viewport_top + viewport_height,
+    	$elem = $(elem),
+    	top = $elem.offset().top,
+    	height = $elem.height(),
+    	bottom = top + height;
 
     return (top >= viewport_top && top < viewport_bottom) ||
            (bottom > viewport_top && bottom <= viewport_bottom) ||
-           (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom)
+           (height > viewport_height && top <= viewport_top && bottom >= viewport_bottom);
   }
 })(jQuery);
